25c15874ff83829ddc8f5609ed8c7330421e6d18
deneb0714-ctrl <deneb0714-ctrl@users.noreply.github.com>
2026-07-13 11:28:59 +0900

    Hide portraits during boss entrance

--- game/hello-world/src/scenes/BossScene.js
+++ game/hello-world/src/scenes/BossScene.js
@@ -1813,6 +1813,7 @@ class BossScene extends Phaser.Scene {
                 await sayEnemyUnknown('「おいおい、こんなところで何してんだ？今引き返すっていうなら見逃してやるぜ？」', 'boss1_normal');
                 await sayDevice('「まずい。魔王軍のやつらに気付かれた。だが、勇者の君なら倒せるだろう。」');
                 
+                this.tweens.add({ targets: [dimBg, enemyFrame, enemyLabel, bossImage, sisterImage, this.heroImage].filter(Boolean), alpha: 0, duration: 300 });
                 // Show boss
                 this.currentBoss.setVisible(true); this.currentBoss.body.enable = true;
                 this.cameras.main.shake(400, 0.015);
@@ -1829,6 +1830,7 @@ class BossScene extends Phaser.Scene {
                 await sayEnemyUnknown('「あは、お客さんだ！」', 'boss2_normal');
                 await sayDevice('「やはり来たか。奴は○○。魔王の狂犬だ。若くして魔王軍に入ったが、魔王の言うこと以外は聞かない。奴は2丁の拳銃を使って戦う。片方だけに気を取られるなよ」');
                 
+                this.tweens.add({ targets: [dimBg, enemyFrame, enemyLabel, bossImage, sisterImage, this.heroImage].filter(Boolean), alpha: 0, duration: 300 });
                 this.currentBoss.setVisible(true); this.currentBoss.body.enable = true;
                 this.cameras.main.shake(400, 0.015);
                 await new Promise(r => this.tweens.add({ targets: this.currentBoss, x: 1400, duration: 1200, ease: 'Power2', onComplete: r }));
@@ -1843,6 +1845,7 @@ class BossScene extends Phaser.Scene {
                 await sayDevice('「…!?お前たちは…」');
                 await sayHero('「？」');
                 
+                this.tweens.add({ targets: [dimBg, enemyFrame, enemyLabel, bossImage, sisterImage, this.heroImage].filter(Boolean), alpha: 0, duration: 300 });
                 this.currentBoss.setVisible(true); this.currentBoss.body.enable = true;
                 this.sisterBoss.setVisible(true); this.sisterBoss.body.enable = true;
                 this.cameras.main.shake(400, 0.015);
